fa717d061649caa465e9ea0dcbfe00ad4743da4d
ZXjogv <zx@jogv.es>
2025-05-02 09:31:54 +0200

    add loader TAP regeneration when building for 128k/+2

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ zcc +zx -v -m -startup=31 -clib=sdcc_iy -SO3 --max-allocs-per-node200000 @zproje
 z88dk-appmake +zx -b intro.scr --org 16384 --noloader --blockname screen -o screen.tap
 z88dk-appmake +zx -b msnampac_CODE.bin --org 25000 --noloader --blockname code -o code.tap
 z88dk-appmake +zx -b msnampac_BANK_6.bin --org 49152 --noloader --blockname bank6 -o bank6.tap
+bas2tap -sdisk -a10 loader.bas loader.tap
 touch msnampac.tap
 rm msnampac.tap
 cat loader.tap screen.tap code.tap bank6.tap > msnampac.tap
