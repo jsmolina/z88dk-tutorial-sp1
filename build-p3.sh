7747150d52690bb5ee093b0fed8da790eeb30d46
jsmolina <jordi.sesmeromolina@telefonica.com>
2025-05-02 23:56:12 +0200

    chore: update master

--- build-p3.sh
+++ build-p3.sh
@@ -5,10 +5,10 @@ z88dk-appmake +zx -b msnampac_CODE.bin --org 25000 --noloader --blockname code -
 z88dk-appmake +zx -b msnampac_BANK_6.bin --org 49152 --noloader --blockname bank6 -o bank6.tap
 bas2tap -sdisk -a10 loader-p3.bas loader-p3.tap
 rm -f disk screen code bank6
-~/src/spectrum/taptools/tapsplit loader-p3.tap
-~/src/spectrum/taptools/tapsplit screen.tap
-~/src/spectrum/taptools/tapsplit code.tap
-~/src/spectrum/taptools/tapsplit bank6.tap
+tapsplit loader-p3.tap
+tapsplit screen.tap
+tapsplit code.tap
+tapsplit bank6.tap
 dskform -type edsk -format cpcdata msnampac.dsk
 cpmcp -f cpcdata msnampac.dsk disk 0:disk
 cpmcp -f cpcdata msnampac.dsk screen 0:screen
